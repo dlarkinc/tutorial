18f126e4b4706df495be4797ce36625d8d913107
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-08 16:16:48 +0100

    cleaning up repo - wrong inited folder

---
+++ src/power.py
@@ -0,0 +1,11 @@
+
+def to_the_power(x, y = 2):
+    result = x
+    for i in range(0, y):
+        result = result * x
+    return result
+
+x = 150
+y = 2
+print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
+
